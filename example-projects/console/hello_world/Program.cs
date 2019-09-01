74948d621efe140a3234a057f1ed2add213a546d
Kurniawan Jatmika <munirjb@gmail.com>
2019-09-01 11:32:31 +0700

    Update Program.cs
    auto reformat lines, add key intercept before exiting

--- example-projects/console/hello_world/Program.cs
+++ example-projects/console/hello_world/Program.cs
@@ -1,7 +1,11 @@
 using System;
 
-class Program{
-    static void Main(String[] args){
+class Program
+{
+    static void Main(String[] args)
+    {
         Console.WriteLine("Hello World!");
+        Console.Write("Press any key to continue...");
+        Console.ReadKey(true);
     }
 }
